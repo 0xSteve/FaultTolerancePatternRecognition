76e53fca89b1f86c62ab81148b3be4f47ef3d149
0xSteve <steven.porretta@gmail.com>
2018-06-21 13:21:41 -0400

    Added python serial monitor.

---
+++ Underwater_Experiment/ElectronicDesign/controller/testADC/serialTest.py
@@ -0,0 +1,22 @@
+import serial
+
+# configure the serial connections (the parameters differs on the device you are connecting to)
+ser = serial.Serial(
+    port='/dev/ttyUSB0',
+    baudrate=9600,
+    parity=serial.PARITY_ODD,
+    stopbits=serial.STOPBITS_TWO,
+    bytesize=serial.SEVENBITS
+)
+
+ser.isOpen()
+
+print('Enter your commands below.\r\nInsert "exit" to leave the application.')
+
+input=1
+while 1 :
+    while ser.inWaiting() > 0:
+        out = ''
+        out += ser.read(1)
+        if out != '':
+            print(">>" + out)
--- Underwater_Experiment/ElectronicDesign/controller/testADC/testADC.ino
+++ Underwater_Experiment/ElectronicDesign/controller/testADC/testADC.ino
@@ -2,30 +2,56 @@
 #include "Adafruit_ADS1015.h"
  
 Adafruit_ADS1015 ads1015;
- 
+Adafruit_ADS1015 ads1015_2(0x49);
+Adafruit_ADS1015 ads1015_3(0x4B);
+
 void setup(void)
 {
   Serial.begin(9600);
   Serial.println("Hello!");
   
-  Serial.println("Getting single-ended readings from AIN0..3");
-  Serial.println("ADC Range: +/- 6.144V (1 bit = 3mV)");
+  Serial.println("Getting single-ended readings");
+  Serial.println("ADC Range: +/- 5.0V (1 bit = 3.11mV)(0-1605)");
   ads1015.begin();
+  ads1015_2.begin();
+  ads1015_3.begin();
 }
  
 void loop(void)
 {
-  int16_t adc0, adc1, adc2, adc3;
+  int16_t adc0, adc1, adc2, adc3, adc10, adc11, adc12, adc13, adc20, adc21,
+  adc22, adc23;
  
   adc0 = ads1015.readADC_SingleEnded(0);
   adc1 = ads1015.readADC_SingleEnded(1);
   adc2 = ads1015.readADC_SingleEnded(2);
   adc3 = ads1015.readADC_SingleEnded(3);
+  float v3, temp3;
+  v3 = (adc3 / 1606.0) * 5;  //typecast to a double.
+  temp3 = (v3 - 0.5) * 100;
+  Serial.print("AIN3 TEMP: "); Serial.println(temp3);
   Serial.print("AIN0: "); Serial.println(adc0);
   Serial.print("AIN1: "); Serial.println(adc1);
   Serial.print("AIN2: "); Serial.println(adc2);
   Serial.print("AIN3: "); Serial.println(adc3);
   Serial.println(" ");
-  
-  delay(1000);
+  adc10 = ads1015_2.readADC_SingleEnded(0);
+  adc11 = ads1015_2.readADC_SingleEnded(1);
+  adc12 = ads1015_2.readADC_SingleEnded(2);
+  adc13 = ads1015_2.readADC_SingleEnded(3);
+  Serial.print("AIN10: "); Serial.println(adc10);
+  Serial.print("AIN11: "); Serial.println(adc11);
+  Serial.print("AIN12: "); Serial.println(adc12);
+  Serial.print("AIN13: "); Serial.println(adc13);
+  Serial.println(" ");
+  adc20 = ads1015_3.readADC_SingleEnded(0);
+  adc21 = ads1015_3.readADC_SingleEnded(1);
+  adc22 = ads1015_3.readADC_SingleEnded(2);
+  adc23 = ads1015_3.readADC_SingleEnded(3);
+  Serial.print("AIN20: "); Serial.println(adc20);
+  Serial.print("AIN21: "); Serial.println(adc21);
+  Serial.print("AIN22: "); Serial.println(adc22);
+  Serial.print("AIN23: "); Serial.println(adc23);
+  Serial.println(" ");
+  delay(5000);
 }

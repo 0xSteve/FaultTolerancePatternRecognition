02a305c919aeee42433cbf2ff6f004ed32befe3e
0xSteve <steven.porretta@gmail.com>
2018-07-02 10:17:09 -0400

    Fixed serial coms.

--- Underwater_Experiment/ElectronicDesign/controller/testADC/serialTest.py
+++ Underwater_Experiment/ElectronicDesign/controller/testADC/serialTest.py
@@ -1,4 +1,5 @@
 import serial
+import time
 
 # configure the serial connections (the parameters differs on the device you are connecting to)
 ser = serial.Serial(
@@ -9,7 +10,29 @@ ser = serial.Serial(
     bytesize=serial.SEVENBITS
 )
 
+######
+#ToDo: set up log file writing
+
+read_line = ''
+write_line = ''
 ser.isOpen()
 while(True):
-    while ser.inWaiting() > 0:
-        print(ser.readline())
+    write_line = input('Need to change? (y/N)').lower()
+    if(write_line == 'q'):
+        ser.close()
+        exit()
+    if(not write_line == 'n'):
+        ser.write('1'.encode())
+        ## IN THIS AREA I NEED TO MENTION DEPTH INCREMENT
+        ser.flush()
+        time.sleep(5)
+        while ser.inWaiting() > 0:
+            print(ser.readline().decode())
+        input('Ready?')
+    else:
+        ser.write('0'.encode())
+        #time.sleep(2)
+        ser.flush()
+        time.sleep(5)
+        while ser.inWaiting() > 0:
+            print(ser.readline().decode())
--- Underwater_Experiment/ElectronicDesign/controller/testADC/testAIN.ino
+++ Underwater_Experiment/ElectronicDesign/controller/testADC/testAIN.ino
@@ -1,36 +1,44 @@
 //#include <Wire.h>
 
-const int AIN0 = A0;
+const int AIN0 = A5;
 const int AIN1 = A1;
 const int AIN2 = A2;
 const int AIN3 = A3;
 const int AIN4 = A4;
 
 float temp1, temp2, temp3, temp4, temp5;
-
+int input = NULL;
 void setup(void)
 {
     Serial.begin(9600);
-    Serial.println("Hello!"); 
-    Serial.println("Getting single-ended readings");
+    //Serial.println("Hello!"); 
+    //Serial.println("Getting single-ended readings");
     //Serial.println("ADC Range: +/- 5.0V (1 bit = 3.11mV)(0-1605)");
-
+    Serial.flush();
 }
  
 void loop(void)
-{
-    Serial.println(analogRead(AIN0));
-    temp1 = ((analogRead(AIN0) / 1024.0) * 5 - 0.5) * 100;
-    temp2 = ((analogRead(AIN1) / 1024.0) * 5 - 0.5) * 100;
-    temp3 = ((analogRead(AIN2) / 1024.0) * 5 - 0.5) * 100;
-    temp4 = ((analogRead(AIN3) / 1024.0) * 5 - 0.5) * 100;
-    temp5 = ((analogRead(AIN4) / 1024.0) * 5 - 0.5) * 100;
+{   
+     while(Serial.available() > 0) {
+        // read the incoming byte:
+        input = Serial.read();
+    //use the following line to debug serial.
+    //Serial.print("You sent: "); Serial.println(input);
+    if(input == 176){
+        temp1 = ((analogRead(AIN0) / 1024.0) * 5 - 0.5) * 100;
+        temp2 = ((analogRead(AIN1) / 1024.0) * 5 - 0.5) * 100;
+        temp3 = ((analogRead(AIN2) / 1024.0) * 5 - 0.5) * 100;
+        temp4 = ((analogRead(AIN3) / 1024.0) * 5 - 0.5) * 100;
+        temp5 = ((analogRead(AIN4) / 1024.0) * 5 - 0.5) * 100;
 
-    Serial.print("TEMPERATURE OF AIN0: "); Serial.println(temp1);
-    Serial.print("TEMPERATURE OF AIN1: "); Serial.println(temp2);
-    Serial.print("TEMPERATURE OF AIN2: "); Serial.println(temp3);
-    Serial.print("TEMPERATURE OF AIN3: "); Serial.println(temp4);
-    Serial.print("TEMPERATURE OF AIN4: "); Serial.println(temp5);
-
-    delay(5000);
+        //Serial.print("AIN0,"); 
+        Serial.print(temp1);
+        Serial.print(","); Serial.print(temp2);
+        Serial.print(","); Serial.print(temp3);
+        Serial.print(","); Serial.print(temp4);
+        Serial.print(","); Serial.println(temp5);
+        Serial.flush();
+        //delay(100);
+    }}
+    
 }
